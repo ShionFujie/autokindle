f712d52343ad2d29e6526c5875557cefce020a8e
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-24 15:06:17 +0900

    multi scheduling

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
-import re
-from event_handlers import EpubFilesHandler
 from observer_runner import ObserverRunner
+from event_handlers import EpubFilesHandler, KindleConnectionHandler
 from watchdog.observers import Observer
-
 
 observer = Observer()
 observer.schedule(
     path=os.path.join(os.environ["HOME"], "Desktop", "To Be Synced"),
     event_handler=EpubFilesHandler()
+)
+observer.schedule(
+    path='/Volumes',
+    event_handler=KindleConnectionHandler()
 )
 ObserverRunner().runObserver(observer)
