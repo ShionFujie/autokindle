eef12c5c6983fdc74fe8ec1e45172b1c43d0b29e
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-24 14:40:42 +0900

    decouple handlers from observers

--- kindle_connection_observer.py
+++ kindle_connection_observer.py
@@ -2,21 +2,14 @@ import os
 import re
 import time
 from observer_runner import ObserverRunner
+from event_handlers import KindleConnectionHandler
 from watchdog.observers import Observer
 from watchdog.events import RegexMatchingEventHandler
 
-def KindleConnectionObserver(path):
-    observer = Observer()
-    observer.schedule(event_handler=_get_kindle_connection_handler(), path=path)
-    return observer
 
-def _get_kindle_connection_handler():
-    def on_any_event(event):
-        type = event.event_type
-        src_path = event.src_path
-        print(f"[{type}] {src_path}")
-    handler = RegexMatchingEventHandler(regexes=[r".*/Kindle"])
-    handler.on_any_event = on_any_event
-    return handler
-
-ObserverRunner().runObserver(KindleConnectionObserver(path='/Volumes'))
+observer = Observer()
+observer.schedule(
+    path='/Volumes',
+    event_handler=KindleConnectionHandler()
+)
+ObserverRunner().runObserver(observer)
