a3147d67fc2bb5ceb0dae38bedfd4bb399910295
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-26 17:46:54 +0900

    state machine

--- main.py
+++ main.py
@@ -1,19 +1,60 @@
 import os
+from store import Store
 from observer_runner import ObserverRunner
 from event_handlers import EpubFilesHandler, KindleConnectionHandler
 from watchdog.observers import Observer
 
 
+class State:
+    def __init__(self, paths=[], is_connected=False):
+        self.paths = paths
+        self.is_connected = is_connected
+
+    def is_idle(self):
+        return not self.paths and not self.is_connected
+
+    def needs_sync(self):
+        return self.paths and not self.is_connected
+
+    def awaits_files(self):
+        return not self.paths and self.is_connected
+
+    def is_syncing(self):
+        return self.paths and self.is_connected
+
+    def copy(self, **updates):
+        return State(**{**self.__dict__, **updates})
+
+
+def reducer(state=State(), action=None):
+    if action is None:
+        return state
+    if action['type'] == 'NEW_FILE':
+        return state.copy(paths=[*state.paths, action['path']])
+    elif action['type'] == 'CONNECTED':
+        return state.copy(is_connected=True)
+    elif action['type'] == 'DISCONNECTED':
+        return state.copy(is_connected=False)
+    else: 
+        return state
+    
+store = Store(reducer)
+
+def on_each():
+    print(store.getState().__dict__)
+
+store.subscribe(on_each)
+
 def on_new_file(path):
-    print("[NEW_FILE] %s" % path)
+    store.dispatch(dict(type='NEW_FILE', path=path))
 
 
 def on_connect():
-    print('[CONNECT]')
+    store.dispatch(dict(type='CONNECTED'))
 
 
 def on_disconnect():
-    print('[DISCONNECT]')
+    store.dispatch(dict(type='DISCONNECTED'))
 
 
 observer = Observer()
