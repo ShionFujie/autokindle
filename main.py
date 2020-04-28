1ee81501da919a737faa0870960838ac8b85c68f
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-28 16:23:27 +0900

    convert pdf to epub

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import subprocess
 from store import Store
 from observer_runner import ObserverRunner
 from event_handlers import EpubFilesHandler, push_new_files, KindleConnectionHandler, push_connection_statuses
 import rx
+from rx import operators
 from watchdog.observers import Observer
 
 
@@ -36,23 +38,44 @@ def reducer(state=State(), action=None):
         return state.copy(is_connected=True)
     elif action['type'] == 'DISCONNECTED':
         return state.copy(is_connected=False)
-    else: 
+    else:
         return state
-    
+
+
 store = Store(reducer)
+
 
 def on_each():
     print(store.getState().__dict__)
 
+
 store.subscribe(on_each)
+
+
+def mapToConvertedFile(new_file):
+    def change_extension_to_epub(path):
+        return f"{os.path.splitext(path)[0]}.epub"
+    def push_converted_files(observer, scheduler):
+        src_path = new_file['path']
+        subprocess.run(
+            [os.path.join(os.environ["HOME"], "Desktop", "room of machinery", "bin", "kindlegen"), src_path], 
+            cwd=os.path.join(os.environ["HOME"], "Desktop", "To Be Synced")
+        )
+        subprocess.run(['rm', src_path])
+        observer.on_next({**new_file, **{'path': change_extension_to_epub(src_path)}})
+        observer.on_completed()
+    return rx.create(push_converted_files)
 
 
 observer = Observer()
 epub_handler = EpubFilesHandler()
 kindle_handler = KindleConnectionHandler()
-new_files = rx.create(push_new_files(epub_handler))
+new_files = rx.create(push_new_files(epub_handler)).pipe(
+    operators.flat_map(mapToConvertedFile)
+)
 connection_statuses = rx.create(push_connection_statuses(kindle_handler))
-rx.merge(new_files, connection_statuses).subscribe(lambda action: store.dispatch(action))
+rx.merge(new_files, connection_statuses).subscribe(
+    lambda action: store.dispatch(action))
 observer.schedule(
     path=os.path.join(os.environ["HOME"], "Desktop", "To Be Synced"),
     event_handler=epub_handler
