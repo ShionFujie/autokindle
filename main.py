3240ae187c0ab566a1f8e85e8ac44ac5631d479f
shion.t.fujie <tonaluna32966@gmail.com>
2020-05-02 18:46:46 +0900

    reorder if-branches

--- main.py
+++ main.py
@@ -36,15 +36,15 @@ class State:
 def reducer(state=State(), action=None):
     if action is None:
         return state
-    if action['type'] == 'NEW_FILE':
+    if action['type'] == 'CONNECTED':
+        return State(is_connected=True, paths=[], processing=state.paths)
+    elif action['type'] == 'DISCONNECTED':
+        return state.copy(is_connected=False)
+    elif action['type'] == 'NEW_FILE':
         if (state.is_connected):
             return state.copy(processing=[action['path']])
         else:
             return state.copy(paths=[*state.paths, action['path']])
-    elif action['type'] == 'CONNECTED':
-        return State(is_connected=True, paths=[], processing=state.paths)
-    elif action['type'] == 'DISCONNECTED':
-        return state.copy(is_connected=False)
     elif action['type'] == 'TRANSFER_FAILED':
         if (state.is_connected):
             return state.copy(processing=[action['path']])
